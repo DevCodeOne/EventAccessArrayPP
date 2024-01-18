0577d0c7d1f5d67201c71b961127007de511bbe7
DevCodeOne <christian.r.dev@googlemail.com>
2024-01-18 19:20:20 +0100

    Initial commit

---
+++ include/event_access_array.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdlib>
+
+namespace EAPP {
+
+namespace Events {
+}
+
+template<typename T, size_t N>
+class EventAccessArray { };
+    static inline constexpr auto Size = N;
+
+}
---
+++ tests/init.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(InitTests, Basic) {
+    EXPECT_EQ(0, 0);
+}
